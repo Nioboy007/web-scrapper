222ac0a74723d7eb97ab79c853a7dd6b735da95e
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-29 13:36:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,82 @@
+import os
+from pyrogram import Client, filters
+from dotenv import load_dotenv
+import os
+from pyrogram.types import Message
+from scraper import all_video_scraping
+from crawler import crawl_web
+from utils import OPTIONS, START_BUTTON, START_TEXT
+
+load_dotenv()
+
+bot_token = os.getenv("BOT_TOKEN")
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+
+CRAWL_LOG_CHANNEL = os.getenv('CRAWL_LOG_CHANNEL')
+
+if bot_token is None or api_id is None or api_hash is None:
+    raise ValueError(
+        "Please set the BOT_TOKEN, API_ID, and API_HASH environment variables."
+    )
+
+app = Client(
+    "WebScrapperBot", bot_token=bot_token, api_id=int(api_id), api_hash=api_hash
+)
+
+
+@app.on_message(filters.command(["start"]))
+async def start(_, message: Message):
+    # Edit Your Start string here
+    text = START_TEXT
+    await message.reply_text(text=text, disable_web_page_preview=True, quote=True)
+
+
+@app.on_callback_query()
+async def cb_data(bot:Client, update):
+    elif update.data == "cballvideo":
+        await all_video_scraping(bot,update)
+    elif update.data == "cballpdf":
+        await all_pdf_scraping(update)
+  
+        else:
+            await update.message.reply('You must provide a Log Channel ID')
+
+    else:
+        await update.message.edit_text(
+            text=START_TEXT, disable_web_page_preview=True, reply_markup=START_BUTTON
+        )
+
+
+@app.on_message(
+    (filters.regex("https") | filters.regex("http") | filters.regex("www"))
+    & filters.private
+)
+async def scrapping(bot, message):
+    await send_message_with_options(message)
+
+
+async def send_message_with_options(message):
+    reply_markup = OPTIONS
+    await message.reply_text("Choose an Option")
+    await message.reply_text(
+        message.text, reply_markup=reply_markup, disable_web_page_preview=True
+    )
+
+
+# Use soup.find_all('tag_name') to Extract Specific Tag Details
+"""
+soup.title
+# <title>This is Title</title>
+
+soup.title.name
+# u'title'
+
+soup.title.string
+# u'This is a string'
+
+soup.title.parent.name
+# u'head'
+"""
+
+app.run(print("Bot Running...."))
