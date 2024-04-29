54e21ccbd06f0bd9bfb4f520bf8c2e126a706ea7
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-29 18:33:53 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -89,28 +89,7 @@ def TimeFormatter(milliseconds: int) -> str:
     return tmp[:-2]
 
 
-async def download_image(base_url, image_url, idx):
-    try:
-        r = requests.get(image_url, stream=True)
-        r.raise_for_status()
-        with open(f"image{idx}.jpg", "wb") as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
-        with open(f"image{idx}.jpg", "rb") as f:
-            image_data = f.read()
-        os.remove(f"image{idx}.jpg")
-        return image_data
-    except requests.exceptions.HTTPError as errh:
-        print(f"HTTP Error: {errh}")
-    except requests.exceptions.ConnectionError as errc:
-        print(f"Error Connecting: {errc}")
-    except requests.exceptions.Timeout as errt:
-        print(f"Timeout Error: {errt}")
-    except requests.exceptions.RequestException as err:
-        print(f"Request Exception: {err}")
-    except Exception as e:
-        print(f"Error downloading image from {image_url}: {e}")
-    return None
+
 
 
 async def download_media(base_url, media_url, idx, media_type):
@@ -134,52 +113,3 @@ async def download_media(base_url, media_url, idx, media_type):
         return None
 
 
-async def download_pdf(base_url, pdf_url, idx, media_type):
-    try:
-        if not pdf_url.startswith(("http:", "https:")):
-            pdf_url = base_url + pdf_url
-        response = requests.get(pdf_url, stream=True)
-        response.raise_for_status()
-
-        # Extract filename from the URL
-        filename = os.path.basename(pdf_url)
-        local_filename = f"{media_type}{idx}_{filename}"
-
-        with open(local_filename, "wb") as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
-
-        with open(local_filename, "rb") as file:
-            pdf_data = file.read()
-
-        os.remove(local_filename)
-        return pdf_data
-
-    except Exception as e:
-        print(f"Error downloading PDF from {pdf_url}: {e}")
-        return None
-
-
-async def init_headless_browser(url):
-    try:
-        options_chrome = Options()
-        options_chrome.headless = True
-        driver_chrome = webdriver.Chrome(options=options_chrome)
-        driver_chrome.get(url)
-        return driver_chrome
-    except Exception as e_chrome:
-        print(
-            f"An error occurred while initializing the Chrome headless browser: {e_chrome}"
-        )
-
-        try:
-            options_firefox = FirefoxOptions()
-            options_firefox.headless = True
-            driver_firefox = webdriver.Firefox(options=options_firefox)
-            driver_firefox.get(url)
-            return driver_firefox
-        except Exception as e_firefox:
-            print(
-                f"An error occurred while initializing the Firefox headless browser: {e_firefox}"
-            )
-            return None
